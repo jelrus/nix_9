e04efc9ea64cf3465c7a9499ccdb29d5d8db7079
jelrus <cvthnmybuthfv1A>
2021-10-30 13:21:28 +0300

    hw_3_start_oop initial commit - minor changes for batch scripts

--- hw_3_start_oop/src/main/resources/batch_scripts/install_lib.bat
+++ hw_3_start_oop/src/main/resources/batch_scripts/install_lib.bat
@@ -1,2 +1,2 @@
 cd ../../../..
-call mvn install:install-file -Dfile=src/main/resources/libs/string_utils.jar -DgroupId=customcollections -DartifactId=custom_collections -Dversion=1.0 -Dpackaging=jar
+call mvn install:install-file -Dfile=src/main/resources/libs/custom_collections.jar -DgroupId=customcollections -DartifactId=custom_collections -Dversion=1.0 -Dpackaging=jar
